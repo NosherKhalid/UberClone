80b9461619f5f07aea24d80bb99841815861c78d
NosherKhalid <nosherkhalid@gmail.com>
2023-01-01 18:55:58 +0500

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Nosher Khalid on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

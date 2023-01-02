1993ff2a344f2470c0bc07013674ec6129b4de1d
NosherKhalid <nosherkhalid@gmail.com>
2023-01-02 20:18:00 +0500

    - Location Search UI

---
+++ UberClone/Core/Home/Views/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  UberClone
+//
+//  Created by Nosher Khalid on 1/1/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            UberMapViewRepresentable()
+                .ignoresSafeArea()
+            LocationSearchActivationView()
+                .padding(.top, 72)
+            MapViewActionButton()
+                .padding(.leading)
+                .padding(.top, 4)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
